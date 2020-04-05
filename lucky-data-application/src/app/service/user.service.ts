4cc4505d03addffea26c04999bbecf7977781f8f
William Trojanowski <williamtrojanowski.wt@gmail.com>
2020-04-06 00:47:32 +0200

    Added IE 9-11 support and removed useless code

--- lucky-data-application/src/app/service/user.service.ts
+++ lucky-data-application/src/app/service/user.service.ts
@@ -3,9 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/user";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
 
   private usersUrl: string;
