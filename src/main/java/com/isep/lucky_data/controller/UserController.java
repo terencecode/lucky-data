3adbc9daece58b24d17c5cc223367458c9ca799f
William Trojanowski <williamtrojanowski.wt@gmail.com>
2020-04-06 00:48:15 +0200

    Secured endpoints in swagger

--- src/main/java/com/isep/lucky_data/controller/UserController.java
+++ src/main/java/com/isep/lucky_data/controller/UserController.java
@@ -4,6 +4,8 @@ import com.isep.lucky_data.configuration.CurrentUser;
 import com.isep.lucky_data.configuration.UserPrincipal;
 import com.isep.lucky_data.payload.request.UserRequest;
 import com.isep.lucky_data.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -18,12 +20,14 @@ public class UserController {
 
     @Secured("ROLE_USER")
     @GetMapping
+    @ApiOperation(value = "Gets the current user" , authorizations = {@Authorization(value = "Bearer")})
     public ResponseEntity<?> getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return ResponseEntity.ok(userService.getCurrentUser(userPrincipal));
     }
 
     @Secured("ROLE_USER")
     @PutMapping
+    @ApiOperation(value = "Updates the user infos" , authorizations = {@Authorization(value = "Bearer")})
     public ResponseEntity<?> updateCurrentUser(@RequestBody UserRequest userRequest, @CurrentUser UserPrincipal userPrincipal) {
         userService.updateCurrentUser(userRequest, userPrincipal);
         return ResponseEntity.ok().build();
